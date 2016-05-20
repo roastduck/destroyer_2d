16dd7adddf3ec6611f9141512ded62e483fc21a4
roastduck <beantang.tang@gmail.com>
2016-05-20 22:16:51 +0800

    help debug in VC

--- src/window.cpp
+++ src/window.cpp
@@ -55,6 +55,13 @@ Window::Window()
         genCursor();
         
         glfwSetKeyCallback(mTarget, key_callback);
+    } catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+        if (mTarget)
+            glfwDestroyWindow(mTarget);
+        glfwTerminate();
+        throw;
     } catch (...)
     {
         if (mTarget)
